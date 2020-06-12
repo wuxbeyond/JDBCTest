0e7fc65f59d42d1b272b988287af38d95595fc19
ranyuan <ran@xs.xom>
2020-06-12 18:34:14 +0800

    Jdbc demo

---
+++ src/main/java/JDBCTest.java
@@ -0,0 +1,56 @@
+import java.sql.*;
+
+public class JDBCTest {
+    public static void main(String[] args) throws SQLException {
+
+        // JDBC用到的三个接口
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        // 数据连接的配置
+        String url = "jdbc:mysql://10.244.76.11:3306/test";
+        String userName = "root";
+        String passWord = "QFIZsy_gNEcd8wAeHV8t9vXgB1AN_ruH";
+
+
+        try {
+
+            // 加载驱动
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // 建立连接
+            connection = DriverManager.getConnection(url, userName, passWord);
+            String sql = "select * from tb_user where  id = ? ";
+
+            // 创建preparedStatement
+            preparedStatement = connection.prepareStatement(sql);
+
+            // 设置参数
+            preparedStatement.setLong(1, 1);
+
+            // 返回执行结果的对象
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("id"));
+                System.out.println(resultSet.getString("user_name"));
+                System.out.println(resultSet.getString("name"));
+
+            }
+
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+}
