efe0acaeea417e7bb1c3fc267420e0f02d10c69f
wuxbeyond <46558152+wuxbeyond@users.noreply.github.com>
2022-10-10 10:39:41 +0800

    测试飞连的DLP功能

---
+++ class.py
@@ -0,0 +1,42 @@
+
+def scope_test():
+    def do_local():
+        spam = "local spam"
+
+    def do_nonlocal():
+        nonlocal spam
+        spam = "nonlocal spam"
+
+    def do_global():
+        global spam
+        spam = "global spam"
+
+    spam = "test spam"
+    do_local()
+    print("After local assignment:", spam)
+    do_nonlocal()
+    print("After nonlocal assignment:", spam)
+    do_global()
+    print("After global assignment:", spam)
+
+
+class Mapping:
+    def __init__(self, iterable):
+        self.items_list = []
+        self.__update(iterable)
+
+    def update(self, iterable):
+        for item in iterable:
+            self.items_list.append(item)
+    __update = update
+
+
+class MappingSubclass(Mapping):
+    def update(self, keys, values):
+        for item in zip(keys, values):
+            self.items_list.append(item)
+
+
+if __name__ == "__main__":
+    scope_test()
+    print("In global scope:", spam)
